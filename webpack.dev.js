7347f96754b8e11ddd0ba5f70c3fc9b16c8435ca
Camila Paschini <camila.paschini@gmail.com>
2018-05-29 17:28:09 +0200

    config adjsutments

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,5 +3,10 @@ const common = require('./webpack.common');
 
 module.exports = merge (common, {
   mode: 'development',
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    quiet: true
+  }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,40 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
-//insert minification plugin here
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge (common, {
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ],
+  },
   plugins: [
-    new UglifyJSPlugin({
-          sourceMap: true
+    new MiniCssExtractPlugin({
+          filename: "[name].css",
+          chunkFilename: "[id].css"
     }),
     new webpack.DefinePlugin({       
       'process.env.NODE_ENV': JSON.stringify('production')
     })
-   ] 
+   ],
+   module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           MiniCssExtractPlugin.loader,
+           "css-loader"
+         ]
+       }
+     ]
+   } 
 });
