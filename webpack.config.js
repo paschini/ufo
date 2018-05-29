21b141986b216c692fb638cd9fb6ad75b5646a9b
Camila Paschini <camila.paschini@gmail.com>
2018-05-29 11:25:51 +0200

    added linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const path = require('path');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+//module.exports = {
+export default {
+  target: 'web',
   devtool:  'inline-source-map',
   entry: [ 
     path.resolve(__dirname, 'src/index') 
@@ -12,8 +16,9 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+      { enforce: "pre", test: /\.js$/, exclude: /node_modules/, loader: "eslint-loader" },
       { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
     ]
   },
   plugins: [
