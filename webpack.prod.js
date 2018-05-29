1010c74c961039c98bc5e6e0cff8a2ef86063b25
Camila Paschini <camila.paschini@gmail.com>
2018-05-29 17:34:50 +0200

    added cleanwebpackplugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const common = require('./webpack.common');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge (common, {
   devtool: 'source-map',
@@ -18,6 +19,7 @@ module.exports = merge (common, {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new MiniCssExtractPlugin({
           filename: "[name].css",
           chunkFilename: "[id].css"
